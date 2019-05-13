6c79b9b537a202feb486e0e68711c3875878887d
Citach <l.acheron01@live.fr>
2019-05-13 10:10:09 +0200

    initial commit

---
+++ src/main/java/com/adaming/ProjetGestionContentieuxApplication.java
@@ -0,0 +1,13 @@
+package com.adaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGestionContentieuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGestionContentieuxApplication.class, args);
+	}
+
+}
